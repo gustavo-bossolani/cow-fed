deb59a74c966e8174a7c843de8fca87e4c4f32ab
gustavo-bossolani <gustavo.wyd@outlook.com>
2023-07-31 10:54:08 -0300

    create get category by filter, create category service mothod

--- src/app/shared/services/category/category.service.ts
+++ src/app/shared/services/category/category.service.ts
@@ -6,6 +6,16 @@ import { environment } from 'src/environments/environment';
 
 import { Category } from '../../models/category/category.model';
 
+interface FilterCategory {
+  keySearch: 'id' | 'name';
+  value: string
+}
+
+export interface CreateCategoryRequest {
+  name: string;
+  color: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -20,4 +30,16 @@ export class CategoryService {
   getCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(`${environment.apiUrl}/${this.categoryRoute}`);
   }
+
+  getCategoryBy({ keySearch, value }: FilterCategory): Observable<Category> {
+    let params: URLSearchParams = new URLSearchParams();
+    params.set(keySearch, value);
+
+    return this.http.get<Category>(`${environment.apiUrl}/${this.categoryRoute}/by?${params.toString()}`)
+  };
+
+  createCategory(category: CreateCategoryRequest): Observable<void> {
+    return this.http.post<void>(`${environment.apiUrl}/${this.categoryRoute}`, category);
+  }
+
 }
