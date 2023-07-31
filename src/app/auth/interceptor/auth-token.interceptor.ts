850bb0802b1a41c99b25c665cf94979fbc93bef4
gustavo-bossolani <gustavo.wyd@outlook.com>
2023-07-31 10:53:11 -0300

    change auth token interceptor for handle unauthorized api calls

--- src/app/auth/interceptor/auth-token.interceptor.ts
+++ src/app/auth/interceptor/auth-token.interceptor.ts
@@ -3,17 +3,23 @@ import {
   HttpRequest,
   HttpHandler,
   HttpInterceptor,
+  HttpErrorResponse,
 } from '@angular/common/http';
-
-import { AuthService } from 'src/app/shared/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 import { environment } from 'src/environments/environment';
+import { catchError } from 'rxjs';
+
+import { AlertType } from 'src/app/shared/components/alert/models/alert.model';
+import { AlertService } from 'src/app/shared/components/alert/services/alert.service';
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
 
 @Injectable()
 export class AuthTokenInterceptor implements HttpInterceptor {
 
   constructor(
-    private authService: AuthService,
+    private alertService: AlertService,
+    private authService: AuthService
   ) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler) {
@@ -26,7 +32,16 @@ export class AuthTokenInterceptor implements HttpInterceptor {
         }
       });
 
-      return next.handle(requestWithHeaders);
+      return next.handle(requestWithHeaders)
+        .pipe(
+          catchError((error: HttpErrorResponse) => {
+            if (error.status === 401) {
+              this.authService.logout();
+              this.alertService.openAlert({ type: AlertType.INFO, message: 'Sessão está expirada, faça o login novamente para seguir.' });
+            }
+            throw new Error('Expired Session.');
+          })
+        );
     }
 
     return next.handle(request);
